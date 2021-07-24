f4d901e305a21b241e868696db356a6c25b7918e
Esther et Fatima <fatima.ezzahra.gabri@gmail.com>
2021-07-24 04:25:43 +0200

    cd commit

---
+++ pySix.py
@@ -0,0 +1,8 @@
+#Challenge 6
+#count(string) the number of syllables of string divided by hyphens and return it.
+def count(string) :
+      return string.count("-") + 1  
+
+def countt(str) :
+    return len(str.split("-"))
+  
